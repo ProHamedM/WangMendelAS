909586e5d41116105b28acdda6912d0dfa70faf4
Hamed <hamed.official2019@gmail.com>
2024-12-16 11:54:25 +0330

    Update FuzzySetGenerator.py

--- FuzzySetGenerator.py
+++ FuzzySetGenerator.py
@@ -1,24 +1,19 @@
 import numpy as np
 import skfuzzy as fuzz
 
+#Handles the generation of triangular membership functions.
 class FuzzySetGenerator:
-    """
-    Handles the generation of triangular membership functions.
-    """
+
     def __init__(self, data_range, n_sets):
         self.data_range = data_range  # Range of values for the variable
         self.n_sets = n_sets  # Number of fuzzy sets per variable
         self.fuzzy_sets = self._generate_triangular_sets()  # Generate fuzzy sets
 
+    # Generate triangular membership functions.
     def _generate_triangular_sets(self):
-        """
-        Generate triangular membership functions.
-        """
         centers = np.linspace(self.data_range[0], self.data_range[-1], self.n_sets)
         return [fuzz.trimf(self.data_range, [c - 1.5, c, c + 1.5]) for c in centers]
 
+    # Return the generated fuzzy sets.
     def get_sets(self):
-        """
-        Return the generated fuzzy sets.
-        """
         return self.fuzzy_sets
